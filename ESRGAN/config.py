Correct the normalization of the dataset
The normalization was previously performed between [0,1], this does not
make sense when apply a LReLU. The normalization is now performed in the
[-1,1] range.

[Ticket: X]

ESRGAN/config.py
@@ -3,13 +3,13 @@ from PIL import Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-LOAD_MODEL = True
+LOAD_MODEL = False
 SAVE_MODEL = True
-SAVE_IMG_CHKPNT = False
+SAVE_IMG_CHKPNT = True
 CHECKPOINT_GEN = "gen.pth.tar"
 CHECKPOINT_DISC = "disc.pth.tar"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-LEARNING_RATE = 4e-6
+LEARNING_RATE = 1e-4
 NUM_EPOCHS = 1000
 BATCH_SIZE = 16
 LAMBDA_GP = 10
@@ -19,13 +19,6 @@ LOW_RES = HIGH_RES // 4
 IMG_CHANNELS = 1
 
 transform = A.Compose(
-    [
-     A.Normalize(mean=[0.5 for _ in range(IMG_CHANNELS)], std=[0.5 for _ in range(IMG_CHANNELS)]),
-     ToTensorV2(),
-    ]
-)
-
-test_transform = A.Compose(
     [
         A.Normalize(mean=[0 for _ in range(IMG_CHANNELS)], std=[1 for _ in range(IMG_CHANNELS)]),
         ToTensorV2(),

ESRGAN/utils.py
@@ -64,7 +64,7 @@ def plot_examples(low_res_folder, gen, target_folder):
 
         with torch.no_grad():
             upscaled_img = gen(
-                config.test_transform(image=np.asarray(test_image))["image"]
+                config.transform(image=np.asarray(test_image))["image"]
                 .unsqueeze(0)
                 .to(config.DEVICE)
             )