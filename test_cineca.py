0ff69bffb2a91fa0d10b0388afede59a79ae04f5
AleD1996 <dianaalessandro96@gmai.com>
2022-03-12 13:16:28 +0100

    Add basic script for Cineca usage

---
+++ test_cineca.py
@@ -0,0 +1,8 @@
+import torch
+
+for i in range(torch.cuda.device_count()):
+    with torch.cuda.device(i):
+        print(f"Device name: {torch.cuda.get_device_name()}")
+        print(f"CUDA capability: {torch.cuda.get_device_capability()}")
+        print(f"Device properties: {torch.cuda.get_device_properties()}")
+        print(f"Is available: {torch.cuda.is_available()}")
